147b91444b56a8ce769755281f8ffcb5ee8a1722
Tadeas Drab <tadeas.drab@student.tuke.sk>
2022-04-24 00:20:16 +0200

    Added fetchers for events

--- data-provider/events/FetchLocations.tsx
+++ data-provider/events/FetchLocations.tsx
@@ -1,5 +1,15 @@
 import { executeQuery } from '../../lib/mysqlDb'
+import { ErrorFetch } from '../../model/ErrorFetch';
 
-export const fetchLocations = async () => {
-    return executeQuery("SELECT * from v_event_locations_names;", []);
+export const fetchLocations = async (): Promise<string[] | ErrorFetch> => {
+    const locations = await executeQuery("SELECT DisplayLocation as DP from v_event_locations_names;", [])
+
+    if (!locations)
+        return { status: 500, type: "Fetch error", message: "Error has occured while selecting locations!" }
+
+    const locs = locations.map((element: any) => {
+        return element.DP
+    });
+
+    return locs;
 }
