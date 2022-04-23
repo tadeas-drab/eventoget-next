accbd6382cb00250b2bbdb84462a741d8a415e77
KatarinaMarcincakova <marcincakova.katarina@gmail.com>
2022-04-23 21:13:53 +0200

    out of container

--- pages/shared/navigation.tsx
+++ pages/shared/navigation.tsx
@@ -11,33 +11,33 @@ const Navigation: NextPage = () => {
             </Head>
             <div className="w-100">
                 <div className="w-100 position-fixed top-0 z-999">
-                    <div className="container">
-                        <div className="row mt-2">
-                            <div className="col-3">
-                                <a className="nav-link"><h3>Tripseek</h3></a>
-                            </div>
-                            <div className="col-1"></div>
-                            <div className="col-8 d-none d-md-block">
-                                <a className="nav-link float-end">Sign up</a>
-                                <a className="nav-link float-end">Log in</a>
-                                <a className="nav-link float-end" href="/#HowItWorks" >How it works</a>
-                                <a className="nav-link float-end">Browse events</a>
-                            </div>
-                            <div className="col-6 d-block d-md-none"></div>
-                            <div className="col-2 d-block d-md-none">
-                                <div className="dropdown">
-                                    <button className="btn dropdown-toggle dropdown-after-none" type="button" id="navMenu" data-bs-toggle="dropdown" aria-expanded="false">
-                                        M
-                                    </button>
-                                    <ul className="dropdown-menu" aria-labelledby="navMenu">
-                                        <li><a className="dropdown-item float-end" href="#">Browse events</a></li>
-                                        <li><a className="dropdown-item float-end" href="#">How it works</a></li>
-                                        <li><a className="dropdown-item float-end" href="#">Log in</a></li>
-                                        <li><a className="dropdown-item float-end" href="#">Sign up</a></li>
-                                    </ul>
-                                </div>
+                    <div className="row mt-2">
+                        <div className="col-1"></div>
+                        <div className="col-2">
+                            <a className="nav-link"><h3>Tripseek</h3></a>
+                        </div>
+                        <div className="col-1"></div>
+                        <div className="col-7 d-none d-md-block">
+                            <a className="nav-link float-end">Sign up</a>
+                            <a className="nav-link float-end">Log in</a>
+                            <a className="nav-link float-end" href="/#HowItWorks" >How it works</a>
+                            <a className="nav-link float-end">Browse events</a>
+                        </div>
+                        <div className="col-4 d-block d-md-none"></div>
+                        <div className="col-2 d-block d-md-none">
+                            <div className="dropdown">
+                                <button className="btn dropdown-toggle dropdown-after-none" type="button" id="navMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                                    M
+                                </button>
+                                <ul className="dropdown-menu" aria-labelledby="navMenu">
+                                    <li><a className="dropdown-item float-end" href="#">Browse events</a></li>
+                                    <li><a className="dropdown-item float-end" href="#">How it works</a></li>
+                                    <li><a className="dropdown-item float-end" href="#">Log in</a></li>
+                                    <li><a className="dropdown-item float-end" href="#">Sign up</a></li>
+                                </ul>
                             </div>
                         </div>
+                        <div className="col-1"></div>
                     </div>
                 </div>
 
