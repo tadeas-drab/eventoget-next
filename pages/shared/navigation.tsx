0950a146257b8ca5f286f536caf4f771c33024da
KatarinaMarcincakova <marcincakova.katarina@gmail.com>
2022-04-23 15:45:03 +0200

    added nav to index

---
+++ pages/shared/navigation.tsx
@@ -0,0 +1,12 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Home.module.css'
+
+const Navigation: NextPage = () => {
+    return (
+        <div className={styles.w100}>asaa</div>
+    )
+}
+
+export default Navigation
