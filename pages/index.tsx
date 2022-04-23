0bc737e9d55eeebb60dcda1983e3fe42e192f306
KatarinaMarcincakova <marcincakova.katarina@gmail.com>
2022-04-23 21:08:55 +0200

    how it works + faq setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -107,6 +107,99 @@ const Home: NextPage = () => {
       
       <div className='bottom-decor'></div>
 
+      <div className='mt-5'>
+        <h1 className='text-center fw-bold fs-55'>How does it work</h1>
+        <div className='container mt-5'>
+          <div className='row m-4'>
+            <div className='col-5'>
+              <h1 className=' text-end'>
+                <img src="/images/search.svg" className='icon'/>
+              </h1>
+            </div>
+            <div className='col-2'></div>
+            <div className='col-5'>
+              <h1 className=''>1. Find event</h1>
+            </div>
+          </div>
+
+          <hr/>
+
+          <div className='row m-4'>
+            <div className='col-5'>
+              <h1 className='text-end'>2. Pick best travel option</h1>
+            </div>
+            <div className='col-2'></div>
+            <div className='col-5'>
+              <h1 className=''>
+                <img src="/images/plane.svg" className='icon'/>
+              </h1>
+            </div>
+          </div>
+
+          <hr/>
+
+          <div className='row m-4'>
+            <div className='col-5'>
+              <h1 className=' text-end'>
+                <img src="/images/happy.svg" className='icon'/>
+              </h1>
+            </div>
+            <div className='col-2'></div>
+            <div className='col-5'>
+              <h1 className=''>Enjoy event</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      
+      <div className='top-decor'></div>
+      <div className='white-bg'>
+        <div className="container pt-3 pb-3">
+          <h1 className='text-center fw-bold fs-55'>FAQ</h1>
+          
+          <div className="accordion" id="accordionExample">
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingOne">
+                <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                  <h3 className='mb-0'>Accordion Item #1</h3>
+                </button>
+              </h2>
+              <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the first item's accordion body.</strong> It is shown by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingTwo">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                  <h3 className='mb-0'>Accordion Item #2</h3>
+                </button>
+              </h2>
+              <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+            <div className="accordion-item">
+              <h2 className="accordion-header" id="headingThree">
+                <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                  <h3 className='mb-0'>Accordion Item #3</h3>
+                </button>
+              </h2>
+              <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                <div className="accordion-body">
+                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='bottom-decor'></div>
+
       <footer className={styles.footer}>
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
