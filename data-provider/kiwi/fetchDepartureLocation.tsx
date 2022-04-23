339d8cbdfd106ac97848c4a92e80bba483e7f5a9
Samuel Neceda <samuel.neceda@gmail.com>
2022-04-24 01:28:04 +0200

    fetchDepartureLocation

--- data-provider/kiwi/fetchDepartureLocation.tsx
+++ data-provider/kiwi/fetchDepartureLocation.tsx
@@ -1,3 +1,30 @@
-export const fetchDepartureLocation = async(userInput: string) => {
-    return userInput;
+const axios = require("axios").default;
+
+const config = {
+  headers: {
+    "Content-Type": "application/json",
+  },
+};
+
+axios.defaults.headers.common = {
+  apikey: "Yv-HrUbVowJVkKPzG50xncA4C-mATFkl",
+};
+
+export const fetchDepartureLocation = async (userInput: string) => {
+  try {
+    const response = await axios.get(
+      `https://tequila-api.kiwi.com/locations/query?term=${userInput}&locale=en-US&location_types=airport&limit=10&active_only=true`,
+      config
+    );
+    const result = [];
+    for (let i = 0; i < response.data.results_retrieved; i++) {
+      result.push([
+        response.data.locations[i].id,
+        response.data.locations[i].name,
+      ]);
+    }
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
 }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -4,6 +4,7 @@ import { fetchArrivalLocation } from "../../data-provider/kiwi/fetchArrivalLocat
 import { fetchFlights } from "../../data-provider/kiwi/fetchFlights";
 import { Flight } from "../../model/Flight";
 import { fetchLocations } from "../../data-provider/events/FetchLocations";
+import { fetchDepartureLocation } from "../../data-provider/kiwi/fetchDepartureLocation";
 
 type Data = {
   name: string;
@@ -17,6 +18,7 @@ export default async function handler(
 
   const arrivalLocation = await fetchArrivalLocation("50.073658", "14.418540"); // Prague coordinates
   console.log(arrivalLocation);
+
   const flights: Flight | undefined = await fetchFlights(
     "KSC",
     arrivalLocation,
@@ -24,6 +26,9 @@ export default async function handler(
     "09/05/2022"
   );
   console.log(flights?.data[0]);
+
+  // const departureLocation = await fetchDepartureLocation("Kos");
+  // console.log(departureLocation);
 
   // res.status(200).json(await fetchLocations());
 }
