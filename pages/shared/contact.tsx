4adba8b8ac208e6bc4504f60955335fb01a33f09
KatarinaMarcincakova <marcincakova.katarina@gmail.com>
2022-04-24 05:33:10 +0200

    changed contact

--- pages/shared/contact.tsx
+++ pages/shared/contact.tsx
@@ -8,7 +8,7 @@ const Contact: NextPage = () => {
     return (
         <div>
             <div className='top-decor'></div>
-            <div className='white-bg pt-5 pb-3'>
+            <div className='white-bg pt-3 pb-2'>
                 <div className='container'>
                     <h2 className='fw-bold mb-3'>Follow us</h2>
                     <div className='row mb-3'>
@@ -23,51 +23,6 @@ const Contact: NextPage = () => {
                         </div>
                         <div className='col-3'>
                             <h3 className=''><Image src="/images/youtube.svg" alt="icon" width={20} height={20}/>&nbsp;Tripseek</h3>
-                        </div>
-                    </div>
-                    <hr className='hr-thin'/>
-                    <div className='row mt-5'>
-                        <div className="col-2">
-                            <Link href="/">
-                                <a className="nav-link animated-underline">
-                                    <h5 className=' fs-085'><span className='animated-underline'>Browse events</span></h5>
-                                </a>
-                            </Link>
-                        </div>
-                        <div className="col-2">
-                            <Link href="/">
-                                <a className="nav-link animated-underline">
-                                    <h5 className=' fs-085'><span className='animated-underline'>How it works</span></h5>
-                                </a>
-                            </Link>
-                        </div>
-                        <div className="col-2">
-                            <Link href="/">
-                                <a className="nav-link animated-underline">
-                                    <h5 className=' fs-085'><span className='animated-underline'>Create account</span></h5>
-                                </a>
-                            </Link>
-                        </div>
-                        <div className="col-2">
-                            <Link href="/">
-                                <a className="nav-link animated-underline">
-                                    <h5 className=' fs-085'><span className='animated-underline'>FAQ</span></h5>
-                                </a>
-                            </Link>
-                        </div>
-                        <div className="col-2">
-                            <Link href="/">
-                                <a className="nav-link animated-underline">
-                                    <h5 className=''><span className='animated-underline'>GDPR</span></h5>
-                                </a>
-                            </Link>
-                        </div>
-                        <div className="col-2">
-                            <Link href="/">
-                                <a className="nav-link animated-underline">
-                                    <h5 className=''><span className='animated-underline'>Terms of use</span></h5>
-                                </a>
-                            </Link>
                         </div>
                     </div>
                     <h5 className='mt-3 text-center mb-0 fs-085'>Made with ♡ by Feature, not a bug</h5>
