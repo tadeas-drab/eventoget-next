b17a2d556a95bc758046e702da086f0f5f972ae6
Tadeas Drab <tadeas.drab@student.tuke.sk>
2022-04-24 14:00:31 +0200

    Test fix

--- pages/api/kiwi/arrivallocations.ts
+++ pages/api/kiwi/arrivallocations.ts
@@ -1,6 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
-import fetchArrivalLocation from '../../../data-provider/kiwi/FetchArrivalLocation';
+import fetchArrivalLoc from '../../../data-provider/kiwi/FetchArrivalLocation';
 
 export default async function handler(
     req: NextApiRequest,
@@ -8,5 +8,5 @@ export default async function handler(
 ) {
     let lat = req.query['lat'].toString();
     let lon = req.query['lon'].toString();
-    res.status(200).json(await fetchArrivalLocation(lat, lon))
+    res.status(200).json(await fetchArrivalLoc(lat, lon))
 }
