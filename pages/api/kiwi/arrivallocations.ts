c02579141bb19807f0f441ed01167faa2faac4a5
Tadeas Drab <tadeas.drab@student.tuke.sk>
2022-04-27 09:31:41 +0200

    Add ts ingore

--- pages/api/kiwi/arrivallocations.ts
+++ pages/api/kiwi/arrivallocations.ts
@@ -1,4 +1,5 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+//@ts-ignore
 import type { NextApiRequest, NextApiResponse } from 'next'
 import fetchArrivalLoc from '../../../data-provider/kiwi/fetcharrivallocation';
 
